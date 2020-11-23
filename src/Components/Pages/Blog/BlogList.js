bcb228618552c0ec40d47fed628fc1f8baf6c08e
munnaSorder <developermunnaa@gmail.com>
2020-11-24 02:02:48 +0600

    blog page update

--- src/Components/Pages/Blog/BlogList.js
+++ src/Components/Pages/Blog/BlogList.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const BlogList = (props) => {
     const {title, blog, image, learnMore} = props.data;
+    const clickLove = () => {
+        alert('Thank you so much for the choice.It is not yet effective. It will be implemented in the next update')
+    }
     return (
             <div className="col-lg-4 mb-4  mx-auto col-container">
                 <div className="card mt-5">
@@ -11,7 +14,7 @@ const BlogList = (props) => {
                         <h5 className="card-title">{title}</h5>
                         <p class="card-text">{blog}</p>
                         <a target="_blank" href={learnMore} className="btn more-btn">Read More</a>
-                        <a href="#"><img align="right" className="love-btn" height="20px" width="20px" src="https://i.ibb.co/LgKgDWX/pngwing-com.png" alt=""/></a>
+                        <a onClick={clickLove} href="#"><img align="right" className="love-btn" height="20px" width="20px" src="https://i.ibb.co/LgKgDWX/pngwing-com.png" alt=""/></a>
                     </div>
                     </div>
                 </div>
