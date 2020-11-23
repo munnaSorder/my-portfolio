f6e116f5ed61d9fb77dd5ec90d2c215a8a98b8d6
munnaSorder <developermunnaa@gmail.com>
2020-11-24 01:37:53 +0600

    blog page update

--- src/Components/Pages/Blog/Blog.js
+++ src/Components/Pages/Blog/Blog.js
@@ -1,45 +1,50 @@
 import React from 'react';
-import image1 from '../../../image/blog/blog1.jpg'
-import image2 from '../../../image/blog/blog2.jpg'
-import image3 from '../../../image/blog/cute.jpg'
-import './Blog.css'
+import './Blog.css';
+import BlogList from './BlogList';
+const blogList = [
+    { 
+        title: '10 important things about React',
+        blog: 'If you are a JavaScript developer then you need to know these 10 things. If you want to be a good JavaScript developer, it is important for you to know these things...',
+        image: 'https://i.ibb.co/Dg4dqr8/blog1.png',
+        learnMore: 'https://freelancermunnaa.medium.com/10-important-things-about-react-6b6350cb08c9',
+    },
+    { 
+        title: 'Discuss the basics of react',
+        blog: 'If you know these things well then you can do well in react. If you want to learn how to react, you need to know about the following things.',
+        image: 'https://i.ibb.co/tCzmS7p/blog2.jpg',
+        learnMore: 'https://freelancermunnaa.medium.com/react-core-concept-da54a9cbe018',
+    },
+    { 
+        title: 'Learn the important issues of JavaScript',
+        blog: 'JavaScript is a popular scripting language used to increase the interactivity and functionality of web pages, form...',
+        image: 'https://i.ibb.co/W5yrbDm/blog3.png',
+        learnMore: 'https://freelancermunnaa.medium.com/learn-the-important-issues-of-javascript-7d9eaa57f56c',
+    },
+    { 
+        title: '10 important things about JavaScript',
+        blog: 'These 10 things about JavaScript are very simple but you will find it very useful...',
+        image: 'https://i.ibb.co/K0LvQgy/blog4.png',
+        learnMore: 'https://freelancermunnaa.medium.com/10-important-things-about-javascript-bc2ae6696fa7',
+    },
+    { 
+        title: 'JAVASCRIPT ARRAY',
+        blog: 'Arrays are list-like objects whose prototype has methods to perform traversal and mutation operations...',
+        image: 'https://appdividend.com/wp-content/uploads/2019/03/Javascript-Array-Includes-Tutorial.png',
+        learnMore: 'https://freelancermunnaa.medium.com/javascript-array-4c4ffe6a1820',
+    }
+]
 
 const Blog = () => {
     return (
-        <div className="container-fluid sdm-bg ">
-           <div className="row m-auto container">
-               <div className="col-md-4 blog-container">
-                    <div class="card card-container">
-                        <img class="card-img-top" height="300px" src={image1} alt="blog" />
-                        <div class="card-body">
-                            <h2 class="card-title text-dark">JavaScript Array</h2>
-                            <p class="card-text text-dark">An array is a special variable, which can hold more than one value at a time...</p>
-                            <a href="https://medium.com/@freelancermunnaa/javascript-array-4c4ffe6a1820" target="_blank" class="btn btn-primary">Read More</a>
-                        </div>
-                    </div>
-               </div>
-               <div className="col-md-4 blog-container">
-                    <div class="card card-container">
-                            <img class="card-img-top" height="300px" src={image2} alt="blog" />
-                            <div class="card-body">
-                                <h2 class="card-title text-dark">How to Type Faster</h2>
-                                <p class="card-text text-dark">There are no secret tips or tricks to becoming a faster typist. That might seem disappointing at ....</p>
-                                <a href="https://www.wikihow.com/Type-Faster" target="_blank" class="btn btn-primary">Read More</a>
-                            </div>
-                    </div>
-               </div>
-               <div className="col-md-4 blog-container">
-                    <div class="card card-container">
-                                <img class="card-img-top" height="300px" src={image3} alt="blog" />
-                                <div class="card-body">
-                                    <h2 class="card-title text-dark">How to Impress a Girl</h2>
-                                    <p class="card-text text-dark">Do you want to wow that certain girl? Though you can't control how she feels and make her fall in love...</p>
-                                    <a href="https://www.wikihow.com/Impress-a-Girl" class="btn btn-primary" target="_blank">Read More</a>
-                                </div>
-                            </div>
-                    </div>
+           <div className="container-fluid sdm-bg">
+               <div className="overlay"></div>
+               <div className="container">
+               <div className="row mx-auto row-container">
+                    {
+                        blogList.map(data => <BlogList data={data} />)
+                    }
+               </div></div>  
            </div>
-        </div>
     );
 };
 
---
+++ src/Components/Pages/Blog/BlogList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const BlogList = (props) => {
+    const {title, blog, image, learnMore} = props.data;
+    return (
+            <div className="col-lg-4 mb-4  mx-auto col-container">
+                <div className="card mt-5">
+                    <div>
+                    <img width="100%"  src={image} alt="" className="card-img-top" />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p class="card-text">{blog}</p>
+                        <a target="_blank" href={learnMore} className="btn more-btn">Read More</a>
+                        <a href="#"><img align="right" className="love-btn" height="20px" width="20px" src="https://i.ibb.co/LgKgDWX/pngwing-com.png" alt=""/></a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default BlogList;
